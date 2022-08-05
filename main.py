20d11dea924fb4779ef8b7d8bb59794093adf2da
Sushmita Jana <sushmita_jana@optum.com>
2022-08-05 13:34:00 +0530

    updated

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def load():
     return render_template('a.html')
-
+@app.route('/snowcare')
+def snowcare():
+   return render_template('snowcare.html')
 
 if __name__ == '__main__':
    app.run(debug = False, host='0.0.0.0')
