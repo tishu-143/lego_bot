eb552ed3ddb11a97721510682a43a4b6f1f5c401
Ghode, Tishant <ghode_tishant@optum.com>
2022-08-05 09:54:38 +0530

    test

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def load():
+    return render_template('a.html')
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
